69b52572763030745bf439f30256f4a3ff4d7d5d
tanishgupta-git <tanishgupta8181@gmail.com>
2021-04-08 22:41:23 +0530

    added a loading state for user to be set

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import ProfileHost from './pages/profileHost/profileHost';
 
 function App({history}){ 
  const [user,Setuser] = useState({token:"",userId:"",typeOfuser:""});
+ const [loading,Setloading] = useState(true);
  const logoutHandler = useCallback(() => {
   Setuser({token: null});
   localStorage.removeItem('token');
@@ -26,32 +27,40 @@ function App({history}){
 useEffect(()=> {
   const token =     localStorage.getItem('token');
   const expiryDate =localStorage.getItem('expiryDate');
+  Setloading(true);
   if (!token || !expiryDate) {
+    Setloading(false);
     return;
   }
   if (new Date(expiryDate) <= new Date()) {
     logoutHandler();
+    Setloading(false);
     return;
   }
   const userId = localStorage.getItem('userId');
   const typeOfuser = localStorage.getItem('typeOfuser');
   Setuser({userId : userId,token : token,typeOfuser:typeOfuser}); 
-
+  Setloading(false);
 },[logoutHandler])
   return (
     <div className="App">
+    { loading ? <span>Loading</span>:
+    <>
     <Header user={user} logoutHandler={logoutHandler}/>
     <Switch>
-    <Route exact path='/user/profile/:userId' render={ (props) => (<ProfileUser {...props} user={user}/> ) } />
+     <Route exact path='/user/profile/:userId' render={ (props) => (<ProfileUser {...props} user={user}/> ) } />
+     <Route path='/user' render={() => (user.token ? (<Redirect to='/' />) : (<UserAuth Setuser={Setuser}/>))}  />
      <Route exact path='/host/profile/:hostId' render={ (props) => (<ProfileHost {...props} user={user}/> ) } />
      <Route path='/host' render={() => ( user.token ? (<Redirect to='/' />) : (<HostAuth Setuser={Setuser}/>))}/>
-     <Route path='/user' render={() => (user.token ? (<Redirect to='/' />) : (<UserAuth Setuser={Setuser}/>))}  />
      <Route exact path='/add-property'  render={(props) => (<AddProperty {...props} user={user}/>)}/>
      <Route exact path='/myproperties'  render={(props) => (<MyProperties {...props} user={user}/>)}/>
      <Route exact path='/properties/:propertyId' render={(props) => (<PropertyDetail {...props} user={user}/>)}/>
      <Route path='/' render={(props) => (<HomePage {...props} user={user}/>)}/>
     </Switch>
+    </>
+    }
     </div>
+
   );
 }
 
